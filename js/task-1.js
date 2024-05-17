21340d754f7516eca79c17b5e271db2d2c1f3350
OlenaZib <155896018+OlenaZib@users.noreply.github.com>
2024-05-17 13:59:14 +0200

    now

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,12 @@
-const liElem = document.querySelectorAll('.item')
+const liElem = document.querySelectorAll('li.item');
+
+console.log(`Namber of categories ${liElem.length}`);
+
+const liElItems = document.querySelectorAll('h2');
+liElItems.forEach(function (liElItem) {
+  const title = liElItem.textContent;
+    console.log(`Categories ${title}`);
+   const numberOfCategories = liElItem.nextElementSibling.querySelectorAll('li').length;
+  console.log(`Elements ${numberOfCategories}`);
+});
+
