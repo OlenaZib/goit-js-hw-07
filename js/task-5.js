1e07c4522e712ada8fb505a901b1b51c037dce3b
OlenaZib <155896018+OlenaZib@users.noreply.github.com>
2024-05-19 16:27:07 +0200

    upd

--- js/task-5.js
+++ js/task-5.js
@@ -2,4 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+const colorElem = document.querySelector('.color');
+const colorBtn = document.querySelector('.change-color');
+
+colorBtn.addEventListener('click', function () {
+  const randomColorElem = getRandomHexColor();
+  document.body.style.backgroundColor = randomColorElem;
+  colorElem.textContent = randomColorElem;
+ 
+});
+
--- js/task-6.js
+++ js/task-6.js
@@ -2,4 +2,43 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+};
+ const controls = document.querySelector(".controls");
+const numberInput = document.querySelector("input[type='number']");
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector("#boxes");
+boxesEl.style.display = 'flex';
+boxesEl.style.flexWrap = 'wrap';
+boxesEl.style.alignItems = 'center';
+boxesEl.style.marginTop = '30px';
+
+numberInput.addEventListener("input", toggleValue);
+createBtn.addEventListener("click", () => {
+    createBoxes(parseInt(inputValue));
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+let inputValue = 0;
+
+function toggleValue(event) {
+    inputValue = event.currentTarget.value;
+    console.log(inputValue);
+}
+
+function createBoxes(amount) {
+    boxesEl.innerHTML = "";
+
+    let boxSize = 30;
+    for (let i = 1; i <= amount; i++) {
+        const newDiv = `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color: ${getRandomHexColor()}"></div>`;
+        boxesEl.insertAdjacentHTML("beforeend", newDiv);
+        boxSize += 10;
+    }
+}
+
+
+function destroyBoxes() {
+    numberInput.value = "";
+    boxesEl.innerHTML = "";
 }
